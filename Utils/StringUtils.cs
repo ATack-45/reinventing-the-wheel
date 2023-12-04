5d8e23fbee71e7197113c141b31a11572d67342f
alec-tackitt-student <144161418+alec-tackitt-student@users.noreply.github.com>
2023-12-04 18:31:06 +0000

    made more string methods

--- Utils/StringUtils.cs
+++ Utils/StringUtils.cs
@@ -5,7 +5,6 @@ namespace MyUtilities {
 
         public static string ToUpper(string input) {
             string outputString = "";
-            int i = 0;
             foreach(char charecter in input) {
                 char outputChar;
                 switch (charecter) {
@@ -92,11 +91,222 @@ namespace MyUtilities {
                         break;
                 }
                 outputString = outputString + outputChar;
-                i++;
             }
-            
             return outputString;
         }
+
+        public static string ToLower(string input) {
+            string outputString = "";
+            foreach(char charecter in input) {
+                char outputChar;
+                switch (charecter) {
+                    case 'A':
+                        outputChar = 'a';
+                        break;
+                    case 'B':
+                        outputChar = 'b';
+                        break;
+                    case 'C':
+                        outputChar = 'c';
+                        break;
+                    case 'D':
+                        outputChar = 'd';
+                        break;
+                    case 'E':
+                        outputChar = 'e';
+                        break;
+                    case 'F':
+                        outputChar = 'f';
+                        break;
+                    case 'G':
+                        outputChar = 'g';
+                        break;
+                    case 'H':
+                        outputChar = 'h';
+                        break;
+                    case 'I':
+                        outputChar = 'i';
+                        break;
+                    case 'J':
+                        outputChar = 'j';
+                        break;
+                    case 'K':
+                        outputChar = 'k';
+                        break;
+                    case 'L':
+                        outputChar = 'l';
+                        break;
+                    case 'M':
+                        outputChar = 'm';
+                        break;
+                    case 'N':
+                        outputChar = 'n';
+                        break;
+                    case 'O':
+                        outputChar = 'o';
+                        break;
+                    case 'P':
+                        outputChar = 'p';
+                        break;
+                    case 'Q':
+                        outputChar = 'q';
+                        break;
+                    case 'R':
+                        outputChar = 'r';
+                        break;
+                    case 'S':
+                        outputChar = 's';
+                        break;
+                    case 'T':
+                        outputChar = 't';
+                        break;
+                    case 'U':
+                        outputChar = 'u';
+                        break;
+                    case 'V':
+                        outputChar = 'v';
+                        break;
+                    case 'W':
+                        outputChar = 'w';
+                        break;
+                    case 'X':
+                        outputChar = 'x';
+                        break;
+                    case 'Y':
+                        outputChar = 'y';
+                        break;
+                    case 'Z':
+                        outputChar = 'z';
+                        break;
+                    default:
+                        outputChar = charecter;
+                        break;
+                }
+                outputString = outputString + outputChar;
+            }
+            return outputString;
+        }
+
+         public static int length(string input) {
+            int i = 0;
+            foreach (char charecter in input) {
+                i++;
+            }
+            return i;
+         }
+
+         public static string Reverse(string input) {
+            int i = MyUtilities.StringUtils.length(input) - 1;
+            string output = "";
+            foreach(char charecter in input) {
+                output= output + input[i];
+                i--;
+            }
+            return output;
+         }
+         
+        public static int CountVowels(string input) {
+            int i = 0;
+            foreach (char charecter in input) {
+                switch (charecter) {
+                    case 'A':
+                        i++;
+                        break;
+                    case 'a':
+                        i++;
+                        break;
+                    case 'E':
+                        i++;
+                        break;
+                    case 'e':
+                        i++;
+                        break;
+                    case 'I':
+                        i++;
+                        break;
+                    case 'i':
+                        i++;
+                        break;
+                    case 'O':
+                        i++;
+                        break;
+                    case 'o':
+                        i++;
+                        break;
+                    case 'U':
+                        i++;
+                        break;
+                    case 'u':
+                        i++;
+                        break;
+                }
+            }
+            return i;
+         }
+
+         public static int CountConsonants(string input) {
+            int i = 0;
+            foreach (char charecter in input) {
+                switch (charecter) {
+                    case 'A':
+                        break;
+                    case 'a':
+                        break;
+                    case 'E':
+                        break;
+                    case 'e':
+                        break;
+                    case 'I':
+                        break;
+                    case 'i':
+                        break;
+                    case 'O':
+                        break;
+                    case 'o':
+                        break;
+                    case 'U':
+                        break;
+                    case 'u':
+                        break;
+                    default:
+                        i++;
+                        break;
+                }
+            }
+            return i;
+         }
+
+        public static bool HasLetter(string input, char letter) {
+            bool isLetter = false;
+            foreach (char charecter in input) {
+                if (charecter == letter) {
+                    isLetter = true;
+                }
+            }
+            return isLetter;
+         }
+         public static char[] ToCharArray(string input) {
+            int i = 0;
+            char[] outputArray = new char[MyUtilities.StringUtils.length(input)];
+            foreach (char charecter in input) {
+                outputArray[i] = charecter;
+                i++;
+            }
+            return outputArray;
+         }
+         public static string Trim(string input) {
+            string outputString = "";
+            foreach (char charecter in input) {
+                if (charecter == ' ') {
+
+                }
+                else {
+                    outputString = outputString + charecter;
+                }
+            }
+            return outputString;
+         }
+
         
     }
 }
