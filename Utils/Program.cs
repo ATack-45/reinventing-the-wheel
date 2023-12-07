cf24792b16470dd224f5d3adcf21cc9b6a557f68
alec-tackitt-student <144161418+alec-tackitt-student@users.noreply.github.com>
2023-12-07 18:55:31 +0000

    finished stringutils

--- Utils/Program.cs
+++ Utils/Program.cs
@@ -1,15 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-string test = "hello";
-string test2 = "HelLo";
-string test3 = "olleh";
-string test4 = "       Hello    !";
-System.Console.WriteLine(MyUtilities.StringUtils.ToUpper(test));
-System.Console.WriteLine(MyUtilities.StringUtils.ToLower(test2));
-System.Console.WriteLine(MyUtilities.StringUtils.length(test));
-System.Console.WriteLine(MyUtilities.StringUtils.Reverse(test3));
-System.Console.WriteLine(MyUtilities.StringUtils.CountVowels(test));
-System.Console.WriteLine(MyUtilities.StringUtils.CountConsonants(test));
-System.Console.WriteLine(MyUtilities.StringUtils.HasLetter(test, 'o'));
-System.Console.WriteLine(MyUtilities.StringUtils.ToCharArray(test));
-System.Console.WriteLine(MyUtilities.StringUtils.Trim(test4));
+// string test = "hello";
+// string test2 = "HelLo";
+// string test3 = "olleh";
+// string test4 = "       Hello    !";
+// System.Console.WriteLine(MyUtilities.StringUtils.ToUpper(test));
+// System.Console.WriteLine(MyUtilities.StringUtils.ToLower(test2));
+// System.Console.WriteLine(MyUtilities.StringUtils.length(test));
+// System.Console.WriteLine(MyUtilities.StringUtils.Reverse(test3));
+// System.Console.WriteLine(MyUtilities.StringUtils.CountVowels(test));
+// System.Console.WriteLine(MyUtilities.StringUtils.CountConsonants(test));
+// System.Console.WriteLine(MyUtilities.StringUtils.HasLetter(test, 'o'));
+// System.Console.WriteLine(MyUtilities.StringUtils.ToCharArray(test));
+// System.Console.WriteLine(MyUtilities.StringUtils.Trim(test4));
+int[] testEncode = MyUtilities.StringUtils.Encode("hello");
+foreach(int num in testEncode){
+    System.Console.WriteLine(num);
+}
+System.Console.WriteLine(MyUtilities.StringUtils.Decode(testEncode));
--- Utils/StringUtils.cs
+++ Utils/StringUtils.cs
@@ -307,6 +307,153 @@ namespace MyUtilities {
             return outputString;
          }
 
+
+        public static string append (string str1, string str2) {
+            string output = str1+ str2;
+            return output;
+        }
+        
+        public static int GetCharNum(char letter){
+            switch (letter) {
+                    case 'a':
+                        return 1;
+                    case 'b':
+                       return 2;
+                    case 'c':
+                        return 3;
+                    case 'd':
+                       return 4;
+                    case 'e':
+                        return 5;
+                    case 'f':
+                        return 6;
+                    case 'g':
+                        return 7;
+                    case 'h':
+                        return 8;
+                    case 'i':
+                        return 9;
+                    case 'j':
+                        return 10;
+                    case 'k':
+                        return 11;
+                    case 'l':
+                        return 12;
+                    case 'm':
+                        return 13;
+                    case 'n':
+                       return 14;
+                    case 'o':
+                        return 15;
+                    case 'p':
+                        return 16;
+                    case 'q':
+                       return 17;
+                    case 'r':
+                        return 18;
+                    case 's':
+                        return 19;
+                    case 't':
+                        return 20;
+                    case 'u':
+                        return 21;
+                    case 'v':
+                        return 22;
+                    case 'w':
+                        return 23;
+                    case 'x':
+                        return 24;
+                    case 'y':
+                        return 25;
+                    case 'z':
+                        return 26;
+                    default:
+                        return -1;
+                }
+        }
+        
+        public static char GetNumChar(int num){
+            switch (num) {
+                    case 1:
+                        return 'a';
+                    case 2:
+                       return 'b';
+                    case 3:
+                        return 'c';
+                    case 4:
+                       return 'd';
+                    case 5:
+                        return 'e';
+                    case 6:
+                        return 'f';
+                    case 7:
+                        return 'g';
+                    case 8:
+                        return 'h';
+                    case 9:
+                        return 'i';
+                    case 10:
+                        return 'j';
+                    case 11:
+                        return 'k';
+                    case 12:
+                        return 'l';
+                    case 13:
+                        return 'm';
+                    case 14:
+                       return 'n';
+                    case 15:
+                        return 'o';
+                    case 16:
+                        return 'p';
+                    case 17:
+                       return 'q';
+                    case 18:
+                        return 'r';
+                    case 19:
+                        return 's';
+                    case 20:
+                        return 't';
+                    case 21:
+                        return 'u';
+                    case 22:
+                        return 'v';
+                    case 23:
+                        return 'w';
+                    case 24:
+                        return 'x';
+                    case 25:
+                        return 'y';
+                    case 26:
+                        return 'z';
+                    default:
+                        return ' ';
+                }
+        }
+        
+        
+        public static int[] Encode(string str) {
+            string lowerString = ToLower(str);
+            int[] stringInNums = new int[length(str)];
+            for (int i = 0; i < length(str); i++) {
+                stringInNums[i] = GetCharNum(str[i]);
+            }
+            int[] encodedString = new int[length(str)];
+            for (int i = 0; i < length(str); i++) {
+                encodedString[i] = stringInNums[i] + 15;
+            }
+            return encodedString;
+        }
+
+        public static string Decode(int[] encoded) {
+            string outputString = "";
+            int length = 0;
+            foreach(int num in encoded) {length++;}
+            for (int i = 0; i < length; i++) {
+                outputString = outputString + GetNumChar(encoded[i]-15);
+            }
+            return outputString;
+        }
         
     }
 }
