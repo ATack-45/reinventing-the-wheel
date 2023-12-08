a6697f16f9fb8353bfb3eab18cf6e2b5a15ab5e2
alec-tackitt-student <144161418+alec-tackitt-student@users.noreply.github.com>
2023-12-08 18:58:31 +0000

    arrayUtils complete

--- Utils/ArrayUtils.cs
+++ Utils/ArrayUtils.cs
@@ -2,5 +2,239 @@ namespace MyUtilities {
 
     public static class ArrayUtils {
         
+        public static int Average(int[] arr) {
+            int sum = 0;
+            int length = 0;
+            int avg;
+            foreach (int num in arr)  {
+                sum += num;
+                length++;
+            }
+            avg = sum/length;
+            return avg;
+        }
+        public static double Average(double[] arr) {
+            double sum = 0;
+            double length = 0;
+            double avg;
+            foreach (double num in arr)  {
+                sum += num;
+                length++;
+            }
+            avg = sum/length;
+            return avg;
+        }
+        public static int Max(int[] arr) {
+            int max = arr[0];
+            foreach (int num in arr)  {
+                if (num > max ) {
+                    max = num;
+                }
+            }
+            return max;
+        }
+        public static double Max(double[] arr) {
+            double max = arr[0];
+            foreach (double num in arr)  {
+                if (num > max ) {
+                    max = num;
+                }
+            }
+            return max;
+        }
+        public static int Min(int[] arr) {
+            int min = arr[0];
+            foreach (int num in arr)  {
+                if (num < min ) {
+                    min = num;
+                }
+            }
+            return min;
+        }
+        public static double Min(double[] arr) {
+            double min = arr[0];
+            foreach (double num in arr)  {
+                if (num < min ) {
+                    min = num;
+                }
+            }
+            return min;
+        }
+        public static int[] SortAscending(int[] arr) { // using selection sort    
+            int minIndex;
+            int minValue; 
+            for (int i = 0; i <arr.Length -1; i++) {
+                    minIndex = i; 
+                    minValue = arr[i]; 
+                    for (int j = i + 1; j < arr.Length; j++){
+                        if (arr[j] < minValue){ 
+                            minValue = arr[j]; 
+                            minIndex = j; 
+                        } 
+                    }
+                    int temp = arr[minIndex]; 
+                    arr[minIndex] = arr[i]; 
+                    arr[i] = temp; 
+            }
+            return arr; 
+        }
+        public static double[] SortAscending(double[] arr) { // using selection sort
+            int minIndex;
+            double minValue; 
+            for (int i = 0; i < Length(arr) -1; i++) {
+                    minIndex = i; 
+                    minValue = arr[i]; 
+                    for (int j = i + 1; j < arr.Length; j++){
+                        if (arr[j] < minValue){ 
+                            minValue = arr[j]; 
+                            minIndex = j; 
+                        } 
+                    }
+                    double temp = arr[minIndex]; 
+                    arr[minIndex] = arr[i]; 
+                    arr[i] = temp; 
+            }
+            return arr; 
+        }
+        public static int[] SortDecending(int[] arr) { // using selection sort    
+            int minIndex;
+            int minValue; 
+            for (int i = 0; i < Length(arr)  -1; i++) {
+                    minIndex = i; 
+                    minValue = arr[i]; 
+                    for (int j = i + 1; j < arr.Length; j++){
+                        if (arr[j] > minValue){ 
+                            minValue = arr[j]; 
+                            minIndex = j; 
+                        } 
+                    }
+                    int temp = arr[minIndex]; 
+                    arr[minIndex] = arr[i]; 
+                    arr[i] = temp; 
+            }
+            return arr; 
+        }
+        public static double[] SortDecending(double[] arr) { // using selection sort
+            int minIndex;
+            double minValue; 
+            for (int i = 0; i <arr.Length -1; i++) {
+                    minIndex = i; 
+                    minValue = arr[i]; 
+                    for (int j = i + 1; j < arr.Length; j++){
+                        if (arr[j] > minValue){ 
+                            minValue = arr[j]; 
+                            minIndex = j; 
+                        } 
+                    }
+                    double temp = arr[minIndex]; 
+                    arr[minIndex] = arr[i]; 
+                    arr[i] = temp; 
+            }
+            return arr; 
+        }
+        public static int Sum(int[] arr) {
+            int sum = 0;
+            foreach (int num in arr)  {
+            sum += num;
+        }
+            return sum;
+        }
+        public static double Sum(double[] arr) {
+            double sum = 0;
+            foreach (double num in arr)  {
+            sum += num;
+        }
+            return sum;
+        }
+        public static int Product(int[] arr) {
+            int product = 1;
+            foreach(int num in arr){
+                product = product * num;
+            }
+            return product;
+        }
+        public static double Product(double[] arr) {
+            double product = 1;
+            foreach(double num in arr){
+                product = product * num;
+            }
+            return product;
+        }
+        public static int Length(int[] arr) {
+            int length = 0;
+            foreach (int num in arr)  {
+                length++;
+            }
+            return length;
+        }
+        public static int Length(double[] arr) {
+            int length = 0;
+            foreach (int num in arr)  {
+                length++;
+            }
+            return length;
+        }
+        public static int[] Reverse(int[] arr) {
+            int[] outputArray = new int[Length(arr)];
+            for (int i = 0; i < Length(arr); i++) {
+                outputArray[Length(arr) -1 - i] = arr[i]; 
+            }
+            return outputArray;
+        }
+        public static double[] Reverse(double[] arr) {
+            double[] outputArray = new double[Length(arr)];
+            for (int i = 0; i < Length(arr); i++) {
+                outputArray[Length(arr) - 1 - i] = arr[i]; 
+            }
+            return outputArray;
+        }
+        public static int[] Clear(int[] arr) {
+            for(int i = 0; i < Length(arr) - 1; i++) {
+                arr[i] = 0;
+            }
+            return arr;
+        }
+        public static double[] Clear(double[] arr, int start, int end) {
+            for(int i = start; i <= end; i++) {
+                arr[i] = 0;
+            }
+            return arr;
+        }
+        public static int IndexOf(int[] arr, int num) {
+            int left = 0;
+            int right = Length(arr) -1;
+
+            while (left <= right){
+                int mid = (left + right) / 2;
+                if (num == arr[mid] ) {
+                    return mid;
+                }
+                else if (arr[mid] < num) {
+                    left = mid + 1;
+                }
+                else {
+                    right = mid - 1;
+                }
+            }
+            return -1;
+        }
+        public static int IndexOf(double[] arr, double num) {
+            int left = 0;
+            int right = Length(arr) -1;
+
+            while (left <= right){
+                int mid = (left + right) / 2;
+                if (num == arr[mid] ) {
+                    return mid;
+                }
+                else if (arr[mid] < num) {
+                    left = mid + 1;
+                }
+                else {
+                    right = mid - 1;
+                }
+            }
+            return -1;
+        }
     }
 }
--- Utils/Program.cs
+++ Utils/Program.cs
@@ -1,20 +1,91 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-// string test = "hello";
-// string test2 = "HelLo";
-// string test3 = "olleh";
-// string test4 = "       Hello    !";
-// System.Console.WriteLine(MyUtilities.StringUtils.ToUpper(test));
-// System.Console.WriteLine(MyUtilities.StringUtils.ToLower(test2));
-// System.Console.WriteLine(MyUtilities.StringUtils.length(test));
-// System.Console.WriteLine(MyUtilities.StringUtils.Reverse(test3));
-// System.Console.WriteLine(MyUtilities.StringUtils.CountVowels(test));
-// System.Console.WriteLine(MyUtilities.StringUtils.CountConsonants(test));
-// System.Console.WriteLine(MyUtilities.StringUtils.HasLetter(test, 'o'));
-// System.Console.WriteLine(MyUtilities.StringUtils.ToCharArray(test));
-// System.Console.WriteLine(MyUtilities.StringUtils.Trim(test4));
+/* stringUtils test cases
+string test = "hello";
+string test2 = "HelLo";
+string test3 = "olleh";
+string test4 = "       Hello    !";
+System.Console.WriteLine(MyUtilities.StringUtils.ToUpper(test));
+System.Console.WriteLine(MyUtilities.StringUtils.ToLower(test2));
+System.Console.WriteLine(MyUtilities.StringUtils.length(test));
+System.Console.WriteLine(MyUtilities.StringUtils.Reverse(test3));
+System.Console.WriteLine(MyUtilities.StringUtils.CountVowels(test));
+System.Console.WriteLine(MyUtilities.StringUtils.CountConsonants(test));
+System.Console.WriteLine(MyUtilities.StringUtils.HasLetter(test, 'o'));
+System.Console.WriteLine(MyUtilities.StringUtils.ToCharArray(test));
+System.Console.WriteLine(MyUtilities.StringUtils.Trim(test4));
 int[] testEncode = MyUtilities.StringUtils.Encode("hello");
 foreach(int num in testEncode){
     System.Console.WriteLine(num);
 }
 System.Console.WriteLine(MyUtilities.StringUtils.Decode(testEncode));
+*/
+
+double[] doubleArr = {5.6,4.7,4,2};
+int[] intArr = {2,6,9,4,2};
+System.Console.WriteLine(MyUtilities.ArrayUtils.Average(doubleArr));
+System.Console.WriteLine(MyUtilities.ArrayUtils.Average(intArr));
+System.Console.WriteLine("");
+
+System.Console.WriteLine(MyUtilities.ArrayUtils.Max(doubleArr));
+System.Console.WriteLine(MyUtilities.ArrayUtils.Max(intArr));
+System.Console.WriteLine("");
+
+System.Console.WriteLine(MyUtilities.ArrayUtils.Min(doubleArr));
+System.Console.WriteLine(MyUtilities.ArrayUtils.Min(intArr));
+System.Console.WriteLine("");
+
+double[] sortedDouble = MyUtilities.ArrayUtils.SortAscending(doubleArr);
+foreach (double num in sortedDouble) { System.Console.Write(num + " ");}
+System.Console.WriteLine("");
+int[] sortedInt = MyUtilities.ArrayUtils.SortAscending(intArr);
+foreach (int num in sortedInt) { System.Console.Write(num + " ");}
+System.Console.WriteLine("");
+System.Console.WriteLine("");
+
+
+double[] sortedDecendingDouble = MyUtilities.ArrayUtils.SortDecending(doubleArr);
+foreach (double num in sortedDecendingDouble) { System.Console.Write(num + " ");}
+System.Console.WriteLine("");
+int[] sortedDecendingInt = MyUtilities.ArrayUtils.SortDecending(intArr);
+foreach (int num in sortedDecendingInt) { System.Console.Write(num + " ");}
+System.Console.WriteLine("");
+System.Console.WriteLine("");
+
+System.Console.WriteLine(MyUtilities.ArrayUtils.Sum(doubleArr));
+System.Console.WriteLine(MyUtilities.ArrayUtils.Sum(intArr));
+System.Console.WriteLine("");
+
+System.Console.WriteLine(MyUtilities.ArrayUtils.Product(doubleArr));
+System.Console.WriteLine(MyUtilities.ArrayUtils.Product(intArr));
+System.Console.WriteLine("");
+
+System.Console.WriteLine(MyUtilities.ArrayUtils.Length(doubleArr));
+System.Console.WriteLine(MyUtilities.ArrayUtils.Length(intArr));
+System.Console.WriteLine("");
+
+double[] ReverseDouble = MyUtilities.ArrayUtils.Reverse(doubleArr);
+foreach (double num in ReverseDouble) { System.Console.Write(num + " ");}
+System.Console.WriteLine("");
+int[] ReverseInt = MyUtilities.ArrayUtils.Reverse(intArr);
+foreach (int num in ReverseInt) { System.Console.Write(num + " ");}
+System.Console.WriteLine("");
+System.Console.WriteLine("");
+
+// double[] ClearedDouble = MyUtilities.ArrayUtils.Clear(doubleArr, 1,3);
+// foreach (double num in ClearedDouble) { System.Console.Write(num + " ");}
+// System.Console.WriteLine("");
+// int[] ClearedInt = MyUtilities.ArrayUtils.Clear(intArr);
+// foreach (int num in ClearedInt) { System.Console.Write(num + " ");}
+// System.Console.WriteLine("");
+// System.Console.WriteLine("");
+
+foreach (int num in intArr) {
+    System.Console.Write(num + " ");
+}
+System.Console.WriteLine("");
+foreach(double num in doubleArr) {
+    System.Console.Write(num + " ");
+}
+System.Console.WriteLine("");
+System.Console.WriteLine(MyUtilities.ArrayUtils.IndexOf(intArr, 4));
+System.Console.WriteLine(MyUtilities.ArrayUtils.IndexOf(doubleArr,4));
