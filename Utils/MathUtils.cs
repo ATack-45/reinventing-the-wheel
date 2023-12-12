cb6a271b894176102af0b716a062878fddbdee21
alec-tackitt-student <144161418+alec-tackitt-student@users.noreply.github.com>
2023-12-12 18:58:11 +0000

    started mathUtils

--- Utils/MathUtils.cs
+++ Utils/MathUtils.cs
@@ -1,6 +1,148 @@
 namespace MyUtilities {
 
-    public static class MathUtils {
+    public static class MathUtils { 
+        public static int Power(int num, int exponent) {
+            int product = num;
+            for (int i = exponent - 1; i > 0; i--){
+                product = product * num;
+            }
+            return product;
+        }
         
+        public static int Abs(int num) {
+            if(num >= 0) {
+                return num;
+            }
+            else {
+                return num * -1;
+            }
+        }
+        public static double Abs(double num) {
+            if(num >= 0) {
+                return num;
+            }
+            else {
+               return num * -1;
+            }
+        }
+        public static float Abs(float num) {
+            if(num >= 0) {
+                return num;
+            }
+            else {
+                return num * -1;
+            }
+        }
+        public static int Min(int num1, int num2) {
+            if ( num1 < num2) {
+                return num1;
+            }
+            else if (num2 < num1) {
+                return num2;
+            }
+            else {
+                return 1;
+            }
+        }
+        public static double Min(double num1, double num2) {
+            if ( num1 < num2) {
+                return num1;
+            }
+            else if (num2 < num1) {
+                return num2;
+            }
+            else {
+                return 1;
+            }
+        }
+        public static float Min(float num1, float num2) {
+            if ( num1 < num2) {
+                return num1;
+            }
+            else if (num2 < num1) {
+                return num2;
+            }
+            else {
+                return 1;
+            }
+        }
+        public static int Max(int num1, int num2) {
+            if ( num1 > num2) {
+                return num1;
+            }
+            else if (num2 > num1) {
+                return num2;
+            }
+            else {
+                return 1;
+            }
+        }
+        public static double Max(double num1, double num2) {
+            if ( num1 > num2) {
+                return num1;
+            }
+            else if (num2 > num1) {
+                return num2;
+            }
+            else {
+                return 1;
+            }
+        }
+        public static float Max(float num1, float num2) {
+            if ( num1 > num2) {
+                return num1;
+            }
+            else if (num2 > num1) {
+                return num2;
+            }
+            else {
+                return 1;
+            }
+        }
+
+        public static int Add(int num1, int num2) {
+            return num1 + num2;
+        }
+        public static double Add(double num1, double num2) {
+            return num1 + num2;
+        }
+        public static float Add(float num1, float num2) {
+            return num1 + num2;
+        }
+        public static int Subtract(int num1, int num2) {
+            return num1 - num2;
+        }
+        public static double Subtract(double num1, double num2) {
+            return num1 - num2;
+        }
+        public static float Subtract(float num1, float num2) {
+            return num1 - num2;
+        }
+        public static int Divide(int num1, int num2) {
+            return num1 / num2;
+        }
+        public static double Divide(double num1, double num2) {
+            return num1 / num2;
+        }
+        public static float Divide(float num1, float num2) {
+            return num1 / num2;
+        }
+
+        public static int Multiply(int num1, int num2) {
+            return num1 * num2;
+        }
+        public static double Multiply(double num1, double num2) {
+            return num1 * num2;
+        }
+        public static float Multiply(float num1, float num2) {
+            return num1 * num2;
+        }
+        public static int Factorial(int num){
+            int product = 1;
+            for(int i = 1; i <= num; i++) {
+                product = product * i;
+            }
+            return product;
+        }
     }
 }
--- Utils/Program.cs
+++ Utils/Program.cs
@@ -20,6 +20,9 @@ foreach(int num in testEncode){
 System.Console.WriteLine(MyUtilities.StringUtils.Decode(testEncode));
 */
 
+
+
+/* arrayUtils test cases
 double[] doubleArr = {5.6,4.7,4,2};
 int[] intArr = {2,6,9,4,2};
 System.Console.WriteLine(MyUtilities.ArrayUtils.Average(doubleArr));
@@ -42,14 +45,14 @@ foreach (int num in sortedInt) { System.Console.Write(num + " ");}
 System.Console.WriteLine("");
 System.Console.WriteLine("");
 
-
-// double[] sortedDecendingDouble = MyUtilities.ArrayUtils.SortDecending(doubleArr);
-// foreach (double num in sortedDecendingDouble) { System.Console.Write(num + " ");}
-// System.Console.WriteLine("");
-// int[] sortedDecendingInt = MyUtilities.ArrayUtils.SortDecending(intArr);
-// foreach (int num in sortedDecendingInt) { System.Console.Write(num + " ");}
-// System.Console.WriteLine("");
-// System.Console.WriteLine("");
+// sort decending
+double[] sortedDecendingDouble = MyUtilities.ArrayUtils.SortDecending(doubleArr);
+foreach (double num in sortedDecendingDouble) { System.Console.Write(num + " ");}
+System.Console.WriteLine("");
+int[] sortedDecendingInt = MyUtilities.ArrayUtils.SortDecending(intArr);
+foreach (int num in sortedDecendingInt) { System.Console.Write(num + " ");}
+System.Console.WriteLine("");
+System.Console.WriteLine("");
 
 System.Console.WriteLine(MyUtilities.ArrayUtils.Sum(doubleArr));
 System.Console.WriteLine(MyUtilities.ArrayUtils.Sum(intArr));
@@ -63,21 +66,22 @@ System.Console.WriteLine(MyUtilities.ArrayUtils.Length(doubleArr));
 System.Console.WriteLine(MyUtilities.ArrayUtils.Length(intArr));
 System.Console.WriteLine("");
 
-// double[] ReverseDouble = MyUtilities.ArrayUtils.Reverse(doubleArr);
-// foreach (double num in ReverseDouble) { System.Console.Write(num + " ");}
-// System.Console.WriteLine("");
-// int[] ReverseInt = MyUtilities.ArrayUtils.Reverse(intArr);
-// foreach (int num in ReverseInt) { System.Console.Write(num + " ");}
-// System.Console.WriteLine("");
-// System.Console.WriteLine("");
+//reverse arrays
+double[] ReverseDouble = MyUtilities.ArrayUtils.Reverse(doubleArr);
+foreach (double num in ReverseDouble) { System.Console.Write(num + " ");}
+System.Console.WriteLine("");
+int[] ReverseInt = MyUtilities.ArrayUtils.Reverse(intArr);
+foreach (int num in ReverseInt) { System.Console.Write(num + " ");}
+System.Console.WriteLine("");
+System.Console.WriteLine("");
 
-// double[] ClearedDouble = MyUtilities.ArrayUtils.Clear(doubleArr, 1,3);
-// foreach (double num in ClearedDouble) { System.Console.Write(num + " ");}
-// System.Console.WriteLine("");
-// int[] ClearedInt = MyUtilities.ArrayUtils.Clear(intArr);
-// foreach (int num in ClearedInt) { System.Console.Write(num + " ");}
-// System.Console.WriteLine("");
-// System.Console.WriteLine("");
+double[] ClearedDouble = MyUtilities.ArrayUtils.Clear(doubleArr, 1,3);
+foreach (double num in ClearedDouble) { System.Console.Write(num + " ");}
+System.Console.WriteLine("");
+int[] ClearedInt = MyUtilities.ArrayUtils.Clear(intArr);
+foreach (int num in ClearedInt) { System.Console.Write(num + " ");}
+System.Console.WriteLine("");
+System.Console.WriteLine("");
 
 
 // MUST comment out all cleared, reversed and SortDecending code to work
@@ -87,3 +91,49 @@ foreach(double num in doubleArr) {System.Console.Write(num + " ");}
 System.Console.WriteLine("");
 System.Console.WriteLine(MyUtilities.ArrayUtils.IndexOf(intArr, 6));
 System.Console.WriteLine(MyUtilities.ArrayUtils.IndexOf(doubleArr,5.6));
+*/
+
+
+System.Console.WriteLine(MyUtilities.MathUtils.Power(2, 3));
+System.Console.WriteLine("");
+
+System.Console.WriteLine(MyUtilities.MathUtils.Abs(-14));
+System.Console.WriteLine(MyUtilities.MathUtils.Abs(-15.5));
+System.Console.WriteLine(MyUtilities.MathUtils.Abs(-15.2f));
+System.Console.WriteLine("");
+
+System.Console.WriteLine(MyUtilities.MathUtils.Min(1,5));
+System.Console.WriteLine(MyUtilities.MathUtils.Min(6.2,5.3));
+System.Console.WriteLine(MyUtilities.MathUtils.Min(3f,2f));
+System.Console.WriteLine("");
+
+System.Console.WriteLine(MyUtilities.MathUtils.Max(1,5));
+System.Console.WriteLine(MyUtilities.MathUtils.Max(6.2,5.3));
+System.Console.WriteLine(MyUtilities.MathUtils.Max(3f,2f));
+System.Console.WriteLine("");
+
+System.Console.WriteLine(MyUtilities.MathUtils.Add(1,5));
+System.Console.WriteLine(MyUtilities.MathUtils.Add(6.2,5.3));
+System.Console.WriteLine(MyUtilities.MathUtils.Add(3f,2f));
+System.Console.WriteLine("");
+
+System.Console.WriteLine(MyUtilities.MathUtils.Subtract(1,5));
+System.Console.WriteLine(MyUtilities.MathUtils.Subtract(6.2,5.3));
+System.Console.WriteLine(MyUtilities.MathUtils.Subtract(3f,2f));
+System.Console.WriteLine("");
+
+System.Console.WriteLine(MyUtilities.MathUtils.Multiply(1,5));
+System.Console.WriteLine(MyUtilities.MathUtils.Multiply(6.2,5.3));
+System.Console.WriteLine(MyUtilities.MathUtils.Multiply(3f,2f));
+System.Console.WriteLine("");
+
+System.Console.WriteLine(MyUtilities.MathUtils.Divide(1,5));
+System.Console.WriteLine(MyUtilities.MathUtils.Divide(6.2,5.3));
+System.Console.WriteLine(MyUtilities.MathUtils.Divide(3f,2f));
+System.Console.WriteLine("");
+
+System.Console.WriteLine(MyUtilities.MathUtils.Factorial(7));
+
+
+
+
