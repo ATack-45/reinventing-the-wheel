0a281b6bec1cd091ca31bb9b690d88b2a3732a6c
alec-tackitt-student <144161418+alec-tackitt-student@users.noreply.github.com>
2024-03-06 17:22:51 +0000

    Added xml comments to all array utils

--- Utils/ArrayUtils.cs
+++ Utils/ArrayUtils.cs
@@ -1,7 +1,11 @@
 namespace MyUtilities {
 
     public static class ArrayUtils {
-        
+        /// <summary>
+        /// Averages the numbers in a given integer array
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
         public static int Average(int[] arr) {
             int sum = 0;
             int length = 0;
@@ -13,6 +17,13 @@ namespace MyUtilities {
             avg = sum/length;
             return avg;
         }
+
+
+        /// <summary>
+        /// Averages numbers in a given double array
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
         public static double Average(double[] arr) {
             double sum = 0;
             double length = 0;
@@ -24,6 +35,12 @@ namespace MyUtilities {
             avg = sum/length;
             return avg;
         }
+
+        /// <summary>
+        /// Finds the largest number in a given integer array
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
         public static int Max(int[] arr) {
             int max = arr[0];
             foreach (int num in arr)  {
@@ -33,6 +50,12 @@ namespace MyUtilities {
             }
             return max;
         }
+
+        /// <summary>
+        /// finds the largest number in a given double array
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
         public static double Max(double[] arr) {
             double max = arr[0];
             foreach (double num in arr)  {
@@ -42,6 +65,12 @@ namespace MyUtilities {
             }
             return max;
         }
+
+        /// <summary>
+        /// finds the smallest number in a given integer array
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
         public static int Min(int[] arr) {
             int min = arr[0];
             foreach (int num in arr)  {
@@ -51,6 +80,12 @@ namespace MyUtilities {
             }
             return min;
         }
+
+        /// <summary>
+        /// finds the smallest number in a given double array
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
         public static double Min(double[] arr) {
             double min = arr[0];
             foreach (double num in arr)  {
@@ -60,6 +95,12 @@ namespace MyUtilities {
             }
             return min;
         }
+
+        /// <summary>
+        /// Sorts a given integer array from smallest to largest using the selection sort method
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
         public static int[] SortAscending(int[] arr) { // using selection sort    
             int minIndex;
             int minValue; 
@@ -78,6 +119,12 @@ namespace MyUtilities {
             }
             return arr; 
         }
+
+         /// <summary>
+        /// Sorts a given double array from smallest to largest using the selection sort method
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
         public static double[] SortAscending(double[] arr) { // using selection sort
             int minIndex;
             double minValue; 
@@ -96,6 +143,12 @@ namespace MyUtilities {
             }
             return arr; 
         }
+
+         /// <summary>
+        /// Sorts a given integer array from largest to smallest using the selection sort method
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
         public static int[] SortDecending(int[] arr) { // using selection sort    
             int minIndex;
             int minValue; 
@@ -114,6 +167,12 @@ namespace MyUtilities {
             }
             return arr; 
         }
+
+         /// <summary>
+        /// Sorts a given double array from largest to smallest using the selection sort method
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
         public static double[] SortDecending(double[] arr) { // using selection sort
             int minIndex;
             double minValue; 
@@ -132,6 +191,12 @@ namespace MyUtilities {
             }
             return arr; 
         }
+
+         /// <summary>
+        /// adds together all integers in a given array and returns the sum
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
         public static int Sum(int[] arr) {
             int sum = 0;
             foreach (int num in arr)  {
@@ -139,6 +204,12 @@ namespace MyUtilities {
         }
             return sum;
         }
+
+        /// <summary>
+        /// adds together all the numbers in a double array and returns the sum
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
         public static double Sum(double[] arr) {
             double sum = 0;
             foreach (double num in arr)  {
@@ -146,6 +217,12 @@ namespace MyUtilities {
         }
             return sum;
         }
+
+        /// <summary>
+        /// multiplies together all the numbers in a int array and returns the product
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
         public static int Product(int[] arr) {
             int product = 1;
             foreach(int num in arr){
@@ -153,6 +230,12 @@ namespace MyUtilities {
             }
             return product;
         }
+
+         /// <summary>
+        /// multiplies together all the numbers in a int array and returns the product
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
         public static double Product(double[] arr) {
             double product = 1;
             foreach(double num in arr){
@@ -160,6 +243,12 @@ namespace MyUtilities {
             }
             return product;
         }
+       
+       /// <summary>
+       /// finds the length of a given integer array
+       /// </summary>
+       /// <param name="arr"></param>
+       /// <returns></returns>
         public static int Length(int[] arr) {
             int length = 0;
             foreach (int num in arr)  {
@@ -167,6 +256,12 @@ namespace MyUtilities {
             }
             return length;
         }
+
+        /// <summary>
+        ///  finds the length of a given double array
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
         public static int Length(double[] arr) {
             int length = 0;
             foreach (int num in arr)  {
@@ -174,6 +269,12 @@ namespace MyUtilities {
             }
             return length;
         }
+
+        /// <summary>
+        /// reverses a given integer array
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
         public static int[] Reverse(int[] arr) {
             int[] outputArray = new int[Length(arr)];
             for (int i = 0; i < Length(arr); i++) {
@@ -181,6 +282,12 @@ namespace MyUtilities {
             }
             return outputArray;
         }
+
+        /// <summary>
+        /// reverses a given double array
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
         public static double[] Reverse(double[] arr) {
             double[] outputArray = new double[Length(arr)];
             for (int i = 0; i < Length(arr); i++) {
@@ -188,18 +295,38 @@ namespace MyUtilities {
             }
             return outputArray;
         }
+
+        /// <summary>
+        /// Clears and set all values in a int array to 0
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
         public static int[] Clear(int[] arr) {
             for(int i = 0; i < Length(arr) - 1; i++) {
                 arr[i] = 0;
             }
             return arr;
         }
+
+
+        /// <summary>
+        /// Clears a range of the given double array and sets all values to 0
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
         public static double[] Clear(double[] arr, int start, int end) {
             for(int i = start; i <= end; i++) {
                 arr[i] = 0;
             }
             return arr;
         }
+
+        /// <summary>
+        /// finds the index of a given number in the given array 
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <param name="num"></param>
+        /// <returns></returns>
         public static int IndexOf(int[] arr, int num) {
             int left = 0;
             int right = Length(arr) -1;
@@ -220,6 +347,13 @@ namespace MyUtilities {
             }
             return -1;
         }
+
+        /// <summary>
+        /// finds the index of a given number in the given double array 
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <param name="num"></param>
+        /// <returns></returns>
         public static int IndexOf(double[] arr, double num) {
             int left = 0;
             int right = Length(arr) -1 ;
