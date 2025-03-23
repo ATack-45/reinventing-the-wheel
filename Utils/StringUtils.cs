a18807319464c52519d7adec9aed3f1efbd78f2d
Alec Tackitt <100729347+ATack-45@users.noreply.github.com>
2025-03-22 21:19:58 -0400

    Update StringUtils.cs

--- Utils/StringUtils.cs
+++ Utils/StringUtils.cs
@@ -3,190 +3,27 @@ namespace MyUtilities {
     public static class StringUtils {
 
 
-        public static string ToUpper(string input) {
+       public static string ToUpper(string input) {
             string outputString = "";
-            foreach(char charecter in input) {
-                char outputChar;
-                switch (charecter) {
-                    case 'a':
-                        outputChar = 'A';
-                        break;
-                    case 'b':
-                        outputChar = 'B';
-                        break;
-                    case 'c':
-                        outputChar = 'C';
-                        break;
-                    case 'd':
-                        outputChar = 'D';
-                        break;
-                    case 'e':
-                        outputChar = 'E';
-                        break;
-                    case 'f':
-                        outputChar = 'F';
-                        break;
-                    case 'g':
-                        outputChar = 'G';
-                        break;
-                    case 'h':
-                        outputChar = 'H';
-                        break;
-                    case 'i':
-                        outputChar = 'I';
-                        break;
-                    case 'j':
-                        outputChar = 'J';
-                        break;
-                    case 'k':
-                        outputChar = 'K';
-                        break;
-                    case 'l':
-                        outputChar = 'L';
-                        break;
-                    case 'm':
-                        outputChar = 'M';
-                        break;
-                    case 'n':
-                        outputChar = 'N';
-                        break;
-                    case 'o':
-                        outputChar = 'O';
-                        break;
-                    case 'p':
-                        outputChar = 'P';
-                        break;
-                    case 'q':
-                        outputChar = 'Q';
-                        break;
-                    case 'r':
-                        outputChar = 'R';
-                        break;
-                    case 's':
-                        outputChar = 'S';
-                        break;
-                    case 't':
-                        outputChar = 'T';
-                        break;
-                    case 'u':
-                        outputChar = 'U';
-                        break;
-                    case 'v':
-                        outputChar = 'V';
-                        break;
-                    case 'w':
-                        outputChar = 'W';
-                        break;
-                    case 'x':
-                        outputChar = 'X';
-                        break;
-                    case 'y':
-                        outputChar = 'Y';
-                        break;
-                    case 'z':
-                        outputChar = 'Z';
-                        break;
-                    default:
-                        outputChar = charecter;
-                        break;
-                }
-                outputString = outputString + outputChar;
+            foreach (char character in input)
+            {
+                // Convert lowercase letters ('a' to 'z') to uppercase using ASCII shift
+                char outputChar = (character >= 'a' && character <= 'z') ? (char)(character - 32) : character;
+                outputString += outputChar;
             }
             return outputString;
         }
-
+        
         public static string ToLower(string input) {
             string outputString = "";
-            foreach(char charecter in input) {
-                char outputChar;
-                switch (charecter) {
-                    case 'A':
-                        outputChar = 'a';
-                        break;
-                    case 'B':
-                        outputChar = 'b';
-                        break;
-                    case 'C':
-                        outputChar = 'c';
-                        break;
-                    case 'D':
-                        outputChar = 'd';
-                        break;
-                    case 'E':
-                        outputChar = 'e';
-                        break;
-                    case 'F':
-                        outputChar = 'f';
-                        break;
-                    case 'G':
-                        outputChar = 'g';
-                        break;
-                    case 'H':
-                        outputChar = 'h';
-                        break;
-                    case 'I':
-                        outputChar = 'i';
-                        break;
-                    case 'J':
-                        outputChar = 'j';
-                        break;
-                    case 'K':
-                        outputChar = 'k';
-                        break;
-                    case 'L':
-                        outputChar = 'l';
-                        break;
-                    case 'M':
-                        outputChar = 'm';
-                        break;
-                    case 'N':
-                        outputChar = 'n';
-                        break;
-                    case 'O':
-                        outputChar = 'o';
-                        break;
-                    case 'P':
-                        outputChar = 'p';
-                        break;
-                    case 'Q':
-                        outputChar = 'q';
-                        break;
-                    case 'R':
-                        outputChar = 'r';
-                        break;
-                    case 'S':
-                        outputChar = 's';
-                        break;
-                    case 'T':
-                        outputChar = 't';
-                        break;
-                    case 'U':
-                        outputChar = 'u';
-                        break;
-                    case 'V':
-                        outputChar = 'v';
-                        break;
-                    case 'W':
-                        outputChar = 'w';
-                        break;
-                    case 'X':
-                        outputChar = 'x';
-                        break;
-                    case 'Y':
-                        outputChar = 'y';
-                        break;
-                    case 'Z':
-                        outputChar = 'z';
-                        break;
-                    default:
-                        outputChar = charecter;
-                        break;
-                }
-                outputString = outputString + outputChar;
+            foreach (char character in input)
+            {
+                // Convert uppercase letters ('A' to 'Z') to lowercase using ASCII shift
+                char outputChar = (character >= 'A' && character <= 'Z') ? (char)(character + 32) : character;
+                outputString += outputChar;
             }
             return outputString;
         }
-
          public static int length(string input) {
             int i = 0;
             foreach (char charecter in input) {
